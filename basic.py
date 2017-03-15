fd45aca28200685d959d814d903649ed4fad0ed2
meawoppl <meawoppl@gmail.com>
2017-03-15 11:47:59 -0700

    WIP

--- basic.py
+++ basic.py
@@ -1,103 +1,168 @@
+#!/usr/bin/env python
+
 import datetime
+import itertools
 import json
 
 from pprint import pprint
 
+import pandas as pd
+from pandas.tseries.offsets import BusinessHour
 import github3
-
+from sklearn import linear_model
+# from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
 from pylab import plot, show, xlabel, ylabel
+from pytz import timezone
 
+LOCAL_TZ = timezone("US/Pacific")
 
-class Counter(dict):
-    def __init__(self, initial=None):
-        if initial is not None:
-            self.update(initial)
-
-    def increment(self, thing, amount=1):
-        self[thing] = self.get(thing, 0) + amount
-
-    def update(self, things):
-        for thing in things:
-            self.increment(thing)
+# pd.set_option('display.height', 500)
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 200)
+pd.set_option("display.width", 240)
 
 
 def print_helpful(thing):
     print(thing)
     pprint(dir(thing))
 
-cfg = json.load(open(".config.json"))
-gh = github3.login(cfg["username"], password=cfg["password"])
-
-user = gh.user("meawoppl")
-
-kesm_repo = gh.repository("3scan", "kesm")
-
 
 def get_prs_since(repo, datetime):
     prs = []
     for n, pr in enumerate(repo.pull_requests(state="closed")):
         print("Scanning:", pr.title)
-        possibly_needs_convestion = pr.created_at.replace(tzinfo=None)
+        needs_consideration = pr.created_at.replace(tzinfo=None)
         # Skip unmerged (closed/wo code)
         if not pr.merged_at:
             print("", "Skipped:", pr.title)
             continue
 
-        if possibly_needs_convestion < datetime:
+        if needs_consideration < datetime:
             break
 
         prs.append(pr)
 
     return prs
 
-a_month_ago = datetime.datetime.now() - datetime.timedelta(weeks=16)
-kesm_prs = get_prs_since(kesm_repo, a_month_ago)
+
+def print_lm(thing1, thing2, description):
+    thing1 = np.asarray(thing1).reshape((-1, 1))
+    thing2 = np.asarray(thing2).reshape((-1, 1))
+    lm = linear_model.LinearRegression().fit(thing1, thing2)
+
+    print(description)
+    print(lm.coef_, lm.intercept_)
 
 
 def compute_files_diff(files_list):
     adds = sum(file.additions for file in files_list)
     dels = sum(file.deletions for file in files_list)
+    return adds, dels, len(files_list)
 
-    return adds, dels
+
+def sort_comments(list_of_comments):
+    return sorted(list_of_comments, key=lambda c: c.created_at)
+
+
+def compute_buisness_hour_delta(start_dt, end_dt):
+    # MRG NOTE: Nasty + slow
+    assert start_dt < end_dt
+    print(start_dt, start_dt.tzinfo, end_dt.tzinfo)
+    for mins in itertools.count():
+        if start_dt + BusinessHour(0) + datetime.timedelta(minutes=mins) > end_dt:
+            break
+    return datetime.timedelta(minutes=mins)
 
 
 def compute_pr_stats(pr_list):
-    # Number of PR's per user
-    user_pr_count = Counter()
-    user_add_lines = Counter()
-    user_del_lines = Counter()
-
+    data_rows = []
     for pr in pr_list:
-        user = pr.user.login
-        user_pr_count.increment(user)
+        # +- of code
+        adds, dels, file_count = compute_files_diff(list(pr.files()))
 
-        adds, dels = compute_files_diff(list(pr.files()))
+        # Sift through comments to get comment count/date
+        review_comments = list(pr.review_comments())
+        issue_comments = list(pr.issue().comments())
+        all_comments = sort_comments(review_comments + issue_comments)
 
-        # pprint(pr.as_dict())
-        user_add_lines.increment(user, adds)
-        user_del_lines.increment(user, dels)
+        # Time created
+        created_dt = pr.created_at.astimezone(LOCAL_TZ)
 
-        time_open = pr.merged_at - pr.created_at
-        review_time = time_open.total_seconds() / (3600 * 24)
-        changes = adds - dels
-        time_per_line_change = changes / review_time
-        print(review_time, adds)
+        # Time merged
+        merged_dt = pr.merged_at.astimezone(LOCAL_TZ)
 
-        plot([review_time], [adds], "go", markersize=15, alpha=0.7)
-        plot([review_time], [dels], "ro", markersize=15, alpha=0.7)
+        # Time to first comment
+        if len(all_comments) == 0:
+            feedback_dt = merged_dt
+        else:
+            feedback_dt = all_comments[0].created_at.astimezone(LOCAL_TZ)
 
-    xlabel("Review Time (Days)")
-    ylabel("Change Count (adds+dels)")
-    show()
-    # Time from open to close
+        feedback_delta = compute_buisness_hour_delta(created_dt, feedback_dt)
+        total_delta = compute_buisness_hour_delta(created_dt, merged_dt)
 
-    for user in user_pr_count:
-        print(user, "+%i-%i" % (user_add_lines[user], user_del_lines[user]))
+        # Review comments
+        comments = len(issue_comments) + len(review_comments)
 
-    return user_pr_count, user_add_lines, user_del_lines
+        data_rows.append((
+            pr.user.login,
+            feedback_delta,
+            total_delta,
+            comments,
+            adds,
+            dels,
+            file_count))
 
+    return pd.DataFrame.from_records(
+        data_rows,
+        columns=(
+            "user",
+            "time_feedback",
+            "time_open",
+            "comments",
+            "adds",
+            "dels",
+            "file_count"))
 
-pprint(compute_pr_stats(kesm_prs))
+cfg = json.load(open(".config.json"))
+gh = github3.login(cfg["username"], password=cfg["password"])
+
+user = gh.user("meawoppl")
+kesm_repo = gh.repository("3scan", "kesm")
+
+this_morning = datetime.datetime.now() - datetime.timedelta(hours=1)
+kesm_prs = get_prs_since(kesm_repo, this_morning)
+kesm_stats = compute_pr_stats(kesm_prs)
+pprint(kesm_stats)
+print("PR's")
+print(kesm_stats.groupby("user").count())
+
+print("Lines added")
+print(kesm_stats.groupby("user")["adds"].sum())
+
+print("Time PR is open")
+print(kesm_stats.groupby("user")["time_open"].sum() / kesm_stats.groupby("user")["time_open"].count())
+
+print("Time to PR feedback")
+print(kesm_stats.groupby("user")["time_feedback"].sum() / kesm_stats.groupby("user")["time_feedback"].count())
+
+print_lm(kesm_stats["time_open"].map(lambda dt: dt.total_seconds() / 3600), kesm_stats["adds"], "add-to")
+print_lm(kesm_stats["time_open"].map(lambda dt: dt.total_seconds() / 3600), kesm_stats["file_count"], "filec-to")
+
+# def print_stats(dataframe):
+#     xlabel("Review Time (Days)")
+#     ylabel("Change Count (adds+dels)")
+
+#     times, comments, adds, dels = np.array(time_effort_adds_dels).T
+
+#     print_lm(adds / 100, times, "Time To Review/HLA")
+#     print_lm(adds / 100, comments, "Revied comments/HLA")
+
+#     for user in user_pr_count:
+#         print(user, "+%i-%i" % (user_add_lines[user], user_del_lines[user]))
+
+#     show()
+#     return user_pr_count, user_add_lines, user_del_lines
 
 
 # def suggest_pr_reviewer(pr):
